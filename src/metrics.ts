2f0055311cf4977810612f78594a53bf71ffac82
day253 <9634619+day253@users.noreply.github.com>
2025-01-09 00:06:53 +0800

    ommit gpu

--- src/metrics.ts
+++ src/metrics.ts
@@ -1,10 +1,6 @@
 import * as si from "systeminformation";
 
 export const metrics = async () => {
-    const gpus = await si.graphics();
-    const levels_gpu = gpus.controllers.map(controller => controller.utilizationGpu);
-    const levels_mem = gpus.controllers.map(controller => controller.utilizationMemory);
-
     const cpuData = await si.currentLoad();
     const cpuUsage = cpuData.currentLoad;
 
@@ -14,19 +10,31 @@ export const metrics = async () => {
     const statusParts = [
         `$(zap)${cpuUsage.toFixed(1)}%`,
         `$(database)${memUsage.toFixed(1)}%`,
-        `$(gpu-usage)${levels_gpu.join(",")}`,
-        `$(gpu-memory)${levels_mem.join(",")}`,
     ];
-
-    let levels_zipped = levels_gpu.map((val, index) => [val, levels_mem[index]]);
 
     const tooltipParts = [
         `CPU: ${cpuUsage.toFixed(1)}%`,
         `MEM: ${memUsage.toFixed(1)}%`,
-        levels_zipped.map((val, index) =>
-            `GPU${index}: gpu: ${val[0]}%, mem: ${val[1]}%`
-        ).join("\n")
     ];
+
+    const gpus = await si.graphics();
+
+    console.log(gpus);
+
+    const levels_gpu = gpus.controllers.map(controller => controller.utilizationGpu);
+    const levels_mem = gpus.controllers.map(controller => controller.utilizationMemory);
+    
+    // if (levels_gpu.length > 0) {
+    //     statusParts.push(`$(gpu-usage)${levels_gpu.join(",")}`);
+    //     let levels_zipped = levels_gpu.map((val, index) => [val, levels_mem[index]]);
+    //     tooltipParts.push(levels_zipped.map((val, index) =>
+    //         `GPU${index}: gpu: ${val[0]}%, mem: ${val[1]}%`
+    //     ).join("\n"));
+    // }
+
+    // if (levels_mem.length > 0) {
+    //     statusParts.push(`$(gpu-memory)${levels_mem.join(",")}`);
+    // }
 
     const text = statusParts.join("");
     const tooltip = tooltipParts.join("\n");
