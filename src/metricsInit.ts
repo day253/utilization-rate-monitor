61699c2130d01fe3843ea58ed6eaea979825968f
day253 <9634619+day253@users.noreply.github.com>
2025-01-09 13:48:30 +0800

    rm i18n

---
+++ src/metricsInit.ts
@@ -0,0 +1,59 @@
+import * as vscode from "vscode";
+import metrics from "./metrics";
+import { getMetrics, MetricsExist } from "./configuration";
+import { MetricCtrProps } from "./constants";
+
+export class Metric {
+	#func: () => Promise<string>;
+	#bar: vscode.StatusBarItem | null = null;
+	#section: MetricsExist;
+
+	constructor({ func, section }: MetricCtrProps) {
+		this.#func = func;
+		this.#section = section;
+	}
+
+	init(index: number) {
+		this.#bar = newBarItem({ priority: -1e3 - index, section: this.#section });
+		this.update();
+		return this;
+	}
+
+	async update() {
+		if (!this.#bar) {
+			throw new Error("Metric not initialized");
+		}
+		this.#bar.text = await this.#func();
+	}
+
+	dispose() {
+		this.#bar?.dispose();
+	}
+}
+
+const newBarItem = ({ priority, section }: { priority: number, section: MetricsExist }) => {
+	const title = `Monitor Pro: ${section}`;
+
+	const sbi = vscode.window.createStatusBarItem(
+		title,
+		vscode.StatusBarAlignment.Left,
+		priority
+	);
+
+	sbi.show();
+	sbi.tooltip = title;
+	sbi.name = sbi.id;
+	return sbi;
+};
+
+const allMetrics = getMetrics.flatMap((x) => {
+	const metric = metrics.find((m) => m.section === x);
+	if (metric) {
+		return new Metric(metric);
+	} else {
+		return [];
+	}
+});
+
+export const getEnabledMetrics = () =>
+	allMetrics.flatMap((x, index) => x.init(index) || []);
